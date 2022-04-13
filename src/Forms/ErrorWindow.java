a2320f705364b8bdb5e3b274600202cc5cf5eee0
Riyufuchi <krevelixtv@gmail.com>
2022-04-14 00:07:18 +0200

    Update 0.1.3 Improved: * Removed duplicit code from class ErrorWindow

--- src/Forms/ErrorWindow.java
+++ src/Forms/ErrorWindow.java
@@ -14,9 +14,9 @@ import Utils.FinalValues;
  * 
  * Project: ODB Manager
  * Created On: 13.07.2020
- * Last Edit: 12.07.2021
+ * Last Edit: 14.04.2022
  * @author Riyufuchi
- * @version 1.3
+ * @version 1.4
  * @since 1.3.5
  */
 
@@ -27,56 +27,46 @@ public class ErrorWindow extends JFrame
 	private JTextArea errorMessageLabel;
 	
 	public ErrorWindow(String windowTitle, String errorMessage)
-    {
-        this.setTitle(windowTitle);
-        this.setMinimumSize(new Dimension(300, 200));
-        this.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-        this.setLocationRelativeTo(null);
-        this.contentPanel = new JPanel(null);
-        setTextArea(errorMessage);
-        this.contentPanel.add(errorMessageLabel);
-        this.add(contentPanel);
-        this.setVisible(true);
-        this.setAlwaysOnTop(true);
-        this.addComponentListener(new ComponentAdapter() 
-        {
-            public void componentResized(ComponentEvent componentEvent) 
-            {
-            	resize();
-            }
-        });
-    }
+	{
+		this.setMinimumSize(new Dimension(300, 200));
+		initErrorWindow(windowTitle, errorMessage);
+	}
 	
 	public ErrorWindow(String windowTitle, int minWidth, int minHeight, String errorMessage)
-    {
-        this.setTitle(windowTitle);
-        this.setMinimumSize(new Dimension(minWidth, minHeight));
-        this.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-        this.setLocationRelativeTo(null);
-        contentPanel = new JPanel(null);
-        setTextArea(errorMessage);
-        contentPanel.add(errorMessageLabel);
-        this.add(contentPanel);
-        this.setVisible(true);
-        this.setAlwaysOnTop(true);
-        this.addComponentListener(new ComponentAdapter() 
-        {
-            public void componentResized(ComponentEvent componentEvent) 
-            {
-            	resize();
-            }
-        });
-    }
+	{
+		this.setMinimumSize(new Dimension(minWidth, minHeight));
+		initErrorWindow(windowTitle, errorMessage);
+	}
+	
+	private void initErrorWindow(String windowTitle, String errorMessage)
+	{
+		this.setTitle(windowTitle);
+		this.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+		this.setLocationRelativeTo(null);
+		this.contentPanel = new JPanel(null);
+		setTextArea(errorMessage);
+		this.add(contentPanel);
+		this.setVisible(true);
+		this.setAlwaysOnTop(true);
+		this.addComponentListener(new ComponentAdapter() 
+		{
+			public void componentResized(ComponentEvent componentEvent) 
+			{
+				resize();
+			}
+		});
+	}
 	
 	private void setTextArea(String message)
 	{
 		errorMessageLabel = new JTextArea();
-        errorMessageLabel.setText(message);
-        errorMessageLabel.setEditable(false);
-        errorMessageLabel.setLineWrap(true);
-        errorMessageLabel.setWrapStyleWord(true);
-        errorMessageLabel.setBackground(FinalValues.DEFAULT_PANE_BACKGROUND);
-        errorMessageLabel.setFont(javax.swing.UIManager.getDefaults().getFont("Label.font"));
+		errorMessageLabel.setText(message);
+		errorMessageLabel.setEditable(false);
+		errorMessageLabel.setLineWrap(true);
+		errorMessageLabel.setWrapStyleWord(true);
+		errorMessageLabel.setBackground(FinalValues.DEFAULT_PANE_BACKGROUND);
+		errorMessageLabel.setFont(javax.swing.UIManager.getDefaults().getFont("Label.font"));
+		contentPanel.add(errorMessageLabel);
 	}
 	
 	private void resize()
